4bb0fd5a5fdd8d10729a3b36e2e84af2d240448c
juniorpinhodev <juniorpinhodev@gmail.com>
2026-02-26 16:52:02 -0300

    feat: 🎸 Melhorar a responsividade Google maps contato

--- app/contato/ContatoClient.jsx
+++ app/contato/ContatoClient.jsx
@@ -99,10 +99,10 @@ const ContatoClient = () => {
               {/* fim item email */}
 
               {/* Google Maps Embed */}
-              <div className="w-full px-6 h-56 rounded-lg overflow-hidden mt-1">
+              <div className="w-full px-6 aspect-video rounded-lg overflow-hidden mt-1">
                 <iframe
                   src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d113017.15615410428!2d-48.798934497830004!3d-27.6275863354394!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x952732f8333575b3%3A0x3b75a564699c43c!2sPalho%C3%A7a%2C%20SC!5e0!3m2!1spt-BR!2sbr!4v1694200000000"
-                  width="80%"
+                  width="100%"
                   height="100%"
                   style={{ border: 0 }}
                   allowFullScreen=""
